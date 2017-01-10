4cb1562b05998018fbf66bde5046849a34005004
holysilent <long_yd@126.com>
2017-01-10 16:00:12 +0800

    edit Constants.java.

--- src/main/java/com/holysilent/jproxy/constant/Constants.java
+++ src/main/java/com/holysilent/jproxy/constant/Constants.java
@@ -1,17 +1,17 @@
 package com.holysilent.jproxy.constant;
 
 /**
- * Created by holysilent on 2017/1/6.
+ * Created by silent on 2017/1/6.
  */
 public class Constants {
 
     /**
-     * 目标服务根地址
+     * target application root address
      */
     public static final String TARGET_APP_BASE_URL = "target-app-base-url";
 
     /**
-     * 初始化配置文件名
+     * init file name
      */
     public static final String INIT_FILE = "init.properties";
 }
